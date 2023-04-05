b79179c3e91d5045e00d881eae0b0989c4190b99
saurabh-huh <115425086+saurabh-huh@users.noreply.github.com>
2023-04-05 10:22:55 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,13 @@
+// import java.util.Arrays;
+public class array {
+    public static void main(String[] args) {
+        
+
+        int[][] finalmarks = {{64,84,89},{63,93,83}};
+        System.out.println(finalmarks[0][0]);
+        System.out.println(finalmarks[1][1]);
+        
+
+    }
+}
+ 
